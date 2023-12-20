0e78e9de03adb389c902f23cdf7ad877c69f6cac
Eunryoung Im <eri12nov@gmail.com>
2023-12-20 12:11:56 +0900

    ddd
    Signed-off-by: Eunryoung Im <eri12nov@gmail.com>

--- Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/mapper/BoardMapper.java
+++ Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/mapper/BoardMapper.java
@@ -1,8 +1,23 @@
 package com.itwillbs.mvc_board.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.itwillbs.mvc_board.vo.BoardVO;
 
 @Mapper
 public interface BoardMapper {
+
+	int insertBoard(BoardVO board);
+
+	List<BoardVO> selectBoardList(
+			@Param("searchType") String searchType, 
+			@Param("searchKeyword") String searchKeyword, 
+			@Param("startRow") int startRow, 
+			@Param("listLimit") int listLimit);
+
+	int selectBoardListCount(@Param("searchType") String searchType, @Param("searchKeyword") String searchKeyword);
 
 }
--- Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/service/BoardService.java
+++ Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/service/BoardService.java
@@ -1,13 +1,31 @@
 package com.itwillbs.mvc_board.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwillbs.mvc_board.mapper.BoardMapper;
+import com.itwillbs.mvc_board.vo.BoardVO;
 
 @Service
 public class BoardService {
 	
 	@Autowired
 	private BoardMapper mapper;
+
+	// 게시물 등록 요청
+	public int registBoard(BoardVO board) {
+		return mapper.insertBoard(board);
+	}
+
+	// 게시물 목록 조회 요청
+	public List<BoardVO> getBoardList(String searchType, String searchKeyword, int startRow, int listLimit) {
+		return mapper.selectBoardList(searchType, searchKeyword, startRow, listLimit);
+	}
+
+	// 전체 게시물 목록 갯수 조회 요청
+	public int getBoardListCount(String searchType, String searchKeyword) {
+		return mapper.selectBoardListCount(searchType, searchKeyword);
+	}
 }
